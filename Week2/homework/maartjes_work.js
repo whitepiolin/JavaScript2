6115fc74b988c91af8c56c60383b2ec5a2fb0fc2
whitepiolin <whitepiolin@gmail.com>
2018-07-27 16:36:19 +0200

    aaa i

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -46,20 +46,20 @@ const tasks = monday.concat(tuesday);
 
 // Add your code here
 //2.2.1
-const tasksDuration = tasks.map((x)=> x.duration /60);
+const tasksDuration = tasks.map((x) => x.duration / 60);
 //2.2.2
-const tasksDurationAbove2 = tasksDuration.filter((x) => x>=2);
+const tasksDurationAbove2 = tasksDuration.filter((x) => x >= 2);
 //2.2.3
-function getSum(total, num) {return total + num;}
+function getSum(total, num) { return total + num; }
 const summing = tasksDurationAbove2.reduce(getSum);
 //2.2.4
-const salary = Math.round(summing*20);
-alert (`Solution1 : Maartje's final salary is €${salary}`);
+const salary = Math.round(summing * 20);
+alert(`Solution1 : Maartje's final salary is €${salary}`);
 
 //In a different and short coding
 const salary2 = tasks
-.map (x => x.duration / 60)
-.filter(x => x >= 2)
-.map(x => x * 20)
-.reduce((total, num) => total + num);
+  .map(x => x.duration / 60)
+  .filter(x => x >= 2)
+  .map(x => x * 20)
+  .reduce((total, num) => total + num);
 alert("Solution 2 with another code : € " + salary2.toFixed(0));
